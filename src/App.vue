0a1a5265acb24d7eb6eaca23e1c295e6d814e8f0
zhouyu <zhouyu@trendytech.com.cn>
2019-01-07 14:00:28 +0800

    提交github演示

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <rudy-captcha font-family="华文新魏 Regular"></rudy-captcha>
-    <Captch font-family="华文琥珀"></Captch>
+    <rudy-captcha font-family="华文琥珀" :click-fresh="true"></rudy-captcha>
+    <!-- <Captch font-family="华文琥珀"></Captch> -->
   </div>
 </template>
 <script>
-import Captch from '../libs/index.js'
+// import Captch from '../libs/index.js'
 export default {
-  components: {Captch}
+  // components: {Captch}
 }
 </script>
